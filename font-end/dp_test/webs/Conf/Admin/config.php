b1adc9dd30ee6e45e78e5cb200858be35809b7eb
liyansong0198 <1559393325@qq.com>
2021-04-19 12:53:32 +0800

    前端提交“ git commit -m前端提交”

---
+++ font-end/dp_test/webs/Conf/Admin/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	//主题配置
+	'DEFAULT_THEME'				=> 'Default',
+	'TMPL_DETECT_THEME'			=> false,						// 自动侦测模板主题
+	'THEME_LIST' 				=> 'Default',					// 模板列表
+    'DEFAULT_CHARSET' 			=> 'utf-8',
+	
+	//页面显示配置
+    'PAGESIZE'              => 20,								// 配置每页显示数据个数
+    'PAGE_NUMBERS'          => 5,								// 分页每页显示分页标签数
+    'ROUND_PAGE'			=> 3,								// 分页上下栏显示的页数	
+)
+?>
